97e7d2678f05168f330d330a031c3ee535338682
Ana Clara <anaclaranoronha.m@gmail.com>
2023-01-05 16:55:25 -0300

    carousel

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -8,4 +8,23 @@ import { Component, OnInit } from '@angular/core';
 export class HomePageComponent {
 
 
+
+  selectCarouselItem(selectedButtonElement:any) {
+
+    const selectedItem = selectedButtonElement.id;
+    const carousel = document.querySelector('.s-cards-carousel');
+    const transform = carousel?.style.transform;
+    const rotateY = transform.match(/rotateY\((-?\d+deg)\)/i);
+    const rotateYDeg = -120 * (Number(selectedItem) - 1);
+    const newTransform = transform.replace(rotateY[0], `rotateY(${rotateYDeg}deg)`);
+
+    carousel.style.transform = newTransform;
+
+    const activeButtonElement = document.querySelector('.s-controller__button--active');
+    activeButtonElement.classList.remove('s-controller__button--active');
+    selectedButtonElement.classList.add('s-controller__button--active');
+
+  }
+
+
 }
