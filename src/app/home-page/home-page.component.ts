23174f793199c511e22723e6e0d0b54d599c3a3e
Ana Clara <anaclaranoronha.m@gmail.com>
2023-01-09 16:15:57 -0300

    img hover

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Directive, HostListener, ElementRef, Input } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
@@ -6,25 +7,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent {
-
-
-
-  selectCarouselItem(selectedButtonElement:any) {
-
-    const selectedItem = selectedButtonElement.id;
-    const carousel = document.querySelector('.s-cards-carousel');
-    const transform = carousel?.style.transform;
-    const rotateY = transform.match(/rotateY\((-?\d+deg)\)/i);
-    const rotateYDeg = -120 * (Number(selectedItem) - 1);
-    const newTransform = transform.replace(rotateY[0], `rotateY(${rotateYDeg}deg)`);
-
-    carousel.style.transform = newTransform;
-
-    const activeButtonElement = document.querySelector('.s-controller__button--active');
-    activeButtonElement.classList.remove('s-controller__button--active');
-    selectedButtonElement.classList.add('s-controller__button--active');
-
-  }
 
 
 }
